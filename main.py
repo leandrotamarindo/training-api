5a2a70c8ecd0f8f5e71748f79a5939968d91a5c1
Leandro Tamarindo <tamarindodiasl@gmail.com>
2026-09-01 20:43:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import requests
 import matplotlib.pyplot as plt
 import pandas as pd
 import smtplib
+import os
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from datetime import date, timedelta
+from dotenv import load_dotenv
+
+load_dotenv()  # carrega o arquivo .env
+
+EMAIL_FROM = os.getenv("EMAIL_FROM")
+EMAIL_TO = os.getenv("EMAIL_TO")
+EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
 
 today = date.today()
 last_month = today - timedelta(days=30)
@@ -18,34 +26,34 @@ params = {
     "to": today.isoformat()
 }
 
-awnser = requests.get(url, params=params)
-data = awnser.json()  
+response = requests.get(url, params=params)
+data = response.json()
 
-df = pd.DataFrame(data)  
+df = pd.DataFrame(data)
 df["date"] = pd.to_datetime(df["date"])
 
 plt.plot(df["date"], df["rate"], marker="o", color="gray")
-plt.title("Dolar price (USD/BRL) - Last 30 days")
+plt.title("Dollar price (USD/BRL) - Last 30 days")
 plt.xlabel("Date")
 plt.ylabel("Price (R$)")
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.savefig("Dolar-price.png")
+plt.savefig("dollar-price.png")
 plt.show()
 
 msg = MIMEMultipart()
-msg["Subject"] = "Diary report - Price USD/BRL"
-msg["From"] = "ltdias07@gmail.com"
-msg["To"] = "tamarindodiasl@gmail.com"
+msg["Subject"] = "Daily report - USD/BRL price"
+msg["From"] = EMAIL_FROM
+msg["To"] = EMAIL_TO
 
-msg.attach(MIMEText("Here is attached the graphic of the price of dolar in the last 30 days.", "plain"))
+msg.attach(MIMEText("Here is attached the graph of the dollar price over the last 30 days.", "plain"))
 
-with open("Dolar-price.png", "rb") as img_file:
+with open("dollar-price.png", "rb") as img_file:
     image = MIMEImage(img_file.read())
-    image.add_header("Content-Disposition", "attachment", filename="dolar-price.png")
+    image.add_header("Content-Disposition", "attachment", filename="dollar-price.png")
     msg.attach(image)
 
 with smtplib.SMTP("smtp.gmail.com", 587) as server:
     server.starttls()
-    server.login("ltdias07@gmail.com", "xrma fdgn ioec inpz")
+    server.login(EMAIL_FROM, EMAIL_APP_PASSWORD)
     server.send_message(msg)
