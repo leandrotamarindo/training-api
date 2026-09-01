63221974bae702b8cdfa1ea6e522b38c7fc1986b
Leandro Tamarindo <tamarindodiasl@gmail.com>
2026-09-01 04:29:09 -0300

    Send to email
    Now the application send the graphic created to my email.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import matplotlib.pyplot as plt
 import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from datetime import date, timedelta
 
 today = date.today()
@@ -20,7 +24,7 @@ data = awnser.json()
 df = pd.DataFrame(data)  
 df["date"] = pd.to_datetime(df["date"])
 
-plt.plot(df["date"], df["rate"], marker="o", color="red")
+plt.plot(df["date"], df["rate"], marker="o", color="gray")
 plt.title("Dolar price (USD/BRL) - Last 30 days")
 plt.xlabel("Date")
 plt.ylabel("Price (R$)")
@@ -28,3 +32,20 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.savefig("Dolar-price.png")
 plt.show()
+
+msg = MIMEMultipart()
+msg["Subject"] = "Diary report - Price USD/BRL"
+msg["From"] = "ltdias07@gmail.com"
+msg["To"] = "tamarindodiasl@gmail.com"
+
+msg.attach(MIMEText("Here is attached the graphic of the price of dolar in the last 30 days.", "plain"))
+
+with open("Dolar-price.png", "rb") as img_file:
+    image = MIMEImage(img_file.read())
+    image.add_header("Content-Disposition", "attachment", filename="dolar-price.png")
+    msg.attach(image)
+
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    server.login("ltdias07@gmail.com", "xrma fdgn ioec inpz")
+    server.send_message(msg)
